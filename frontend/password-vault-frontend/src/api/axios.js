a95290e3f56d190def87210be59b698dd1199ea5
saloni20337 <salonikumari4726@gmail.com>
2026-09-11 06:49:56 +0530

    Change API base URL to production endpoint

--- frontend/password-vault-frontend/src/api/axios.js
+++ frontend/password-vault-frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://password-vaults-credential.onrender.com/api",
 });
-
 api.interceptors.request.use((config) => {
 
   const token = localStorage.getItem("token");
